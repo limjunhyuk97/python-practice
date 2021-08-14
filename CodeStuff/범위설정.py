9295eb97e0948ca75ec6fe5374500cd0ac8f5522
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-08-14 15:49:39 +0900

    Create 범위설정.py

---
+++ CodeStuff/범위설정.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+## [10 <= n and n <= 90] == [10 <= n <= 90]
+if 10<= n <= 90:
+    print("WOW")
+
+## 드모르간에 따라서 not (10 > n or n > 90)
+if not(10>n or n>90):
+    print("WOW")
+
+
+
+
+
