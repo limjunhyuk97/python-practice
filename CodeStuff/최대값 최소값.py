58a8eac459c3e72d3f34b5b2620f0a91cba44d4a
Jun Hyuk Lim <limjunhyuk97@naver.com>
2021-08-23 12:25:18 +0900

    Update 최대값 최소값.py

--- CodeStuff/최대값 최소값.py
+++ CodeStuff/최대값 최소값.py
@@ -1,7 +1,6 @@
 # N개 중 가장 큰 값
-# N개 중 가장 큰 값
 
-## 함수 구현
+## 0. 함수 구현
 def maxN(*args):
     max = 0
     for num in args:
@@ -11,6 +10,31 @@ def maxN(*args):
 
 print(maxN(2,3,4,5,6))
 
+## 1. 함수 구현
+import random
+from typing import Any, Sequence
+
+def max_of(a : Sequence) -> Any:
+    maximum = a[0]
+    for i in range(len(a)):
+        if maximum < a[i]:
+            maximum = a[i]
+    return maximum
+
+if __name__ == '__main__':
+    print('배열의 최대값을 구합니다.')
+    num = int(input('원소 수를 입력 하세요 : '))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = random.randint(0, 200)
+        print(x[i], ' ', end='')
+
+    print(f'\n최대값은 {max_of(x)} 입니다.')
+
+
+    
+    
 # min / max 함수 + 'key=' 조건 이용하기
 
 ## 1. 리스트에서 최대값 / 최소값
@@ -20,6 +44,7 @@ print(min(list))
 print(max([num for num in range(1, 20)], key = lambda x : x % 4 and x))
 ## 8 2 19
 
+
 ## 2. 딕셔너리에서 최대 value key / 최소 value key
 dict = {1:10, 2: 20, 3: 30, 4:23, 5: 9}
 print(max(dict, key = dict.__getitem__))
@@ -27,4 +52,4 @@ print(min(dict, key = dict.__getitem__))
 ## 3 5
 
 ### __getitem__ : 특정 객체내의 요소들을 슬라이싱하기 위한 메소드이다.
-### max, min 에서의 key : 정렬의 기준을 사용자가 설정할 수 있게 해준다.
+### max, min 에서의 key : key로 넣은 기준으로 max, min 값을 도출해 낸다.
